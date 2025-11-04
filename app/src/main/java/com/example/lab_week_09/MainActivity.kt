a4c144128dfb1d24d2d43b0774ba31e042388be0
Yoga Wyas <49479642+yogawyas@users.noreply.github.com>
2025-11-04 12:17:31 +0700

    Commit No. 1 – Building Jetpack Compose UI

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -3,27 +3,35 @@ package com.example.lab_week_09
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //Here, we use setContent instead of setContentView
         setContent {
+            //Here, we wrap our content with the theme
             LAB_WEEK_09Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val list = listOf("Tanu", "Tina", "Tono")
+                    //Here, we call the Home composable
+                    Home(list)
                 }
             }
         }
@@ -31,17 +39,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Home(items: List<String>) {
+    //Here, we use LazyColumn to lazily display a list of items vertically
+    LazyColumn {
+        item {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(id = R.string.enter_item))
+
+                //Here, we use TextField to display a text input field
+                TextField(
+                    value = "",
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    ),
+                    onValueChange = { }
+                )
+
+                //Here, we use Button to display a button
+                Button(onClick = { }) {
+                    Text(text = stringResource(id = R.string.button_click))
+                }
+            }
+        }
+
+        //Here, we use items to display a list of items inside the LazyColumn
+        items(items) { item ->
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = item)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LAB_WEEK_09Theme {
-        Greeting("Android")
-    }
+fun PreviewHome() {
+    Home(listOf("Tanu", "Tina", "Tono"))
 }
